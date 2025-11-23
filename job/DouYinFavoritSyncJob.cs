b4cee4f3589d437498991e03d9c08afdf590ad28
jianzhichu <jianzhichu@qq.com>
2025-11-23 23:07:42 +0800

    1

--- job/DouYinFavoritSyncJob.cs
+++ job/DouYinFavoritSyncJob.cs
@@ -58,7 +58,6 @@ namespace dy.net.job
         {
             var bitRate = item.Video.BitRate.FirstOrDefault();
             return $"{item.AwemeId}.{bitRate.Format}";
-
         }
 
         protected override string GetAuthorAvatarBasePath(DouyinUserCookie cookie)
